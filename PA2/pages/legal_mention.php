a4bdca32097c2e9c00526fb51e03d916f767cdb7
Ryatozz <ryatozz@gmail.com>
2024-03-23 22:35:18 +0100

    Add legal mention and policy pages

---
+++ PA2/pages/legal_mention.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Mentions légales</title>
+</head>
+<body>
+<?php include('../includes/header/headerNoConnexion/headerAccueil.php') ?>
+    
+    <h1>Mentions légales</h1>
+    <p>
+        
+        Nom de l'association : Au Temps Donné<br>
+        Adresse : 6 Boulevard Gambetta<br>
+        Code postal - Ville : 02100 - SAINT-QUENTIN<br>
+        Téléphone : +33 (0)1 23 45 67 89<br>
+        Email : contact@autempsdonne.com<br>
+        Siret : 123456789012345<br>
+        APE : 45678<br>
+        
+    </p>
+    <h2>Hébergement</h2>
+    <p>
+        Nom de l'hébergeur : OVH<br>
+        Adresse : 2 rue Kellermann<br>
+        Code postal - Ville : 59053 - Roubaix<br>
+        Téléphone : +33 (0)1 23 45 67 89<br>
+        Email : contact@ovh.com<br>
+    </p>
+    <h2>Propriété intellectuelle</h2>
+    <p>
+        Le contenu de ce site est protégé par les lois en vigueur sur la propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l'éditeur.
+    </p>
+    <h2>Données personnelles</h2>
+    <p>
+        En application de la loi informatique et libertés du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification, de suppression et d'opposition aux données personnelles vous concernant. Pour exercer ces droits, veuillez contacter l'éditeur du site à l'adresse indiquée ci-dessus.
+    </p>
+    <h2>Liens hypertextes</h2>
+    <p>
+        Le site peut contenir des liens hypertextes vers d'autres sites. L'éditeur du site n'est pas responsable du contenu de ces sites et ne peut être tenu responsable des dommages directs ou indirects consécutifs à la consultation de ces sites.
+    </p>
+    <?php include('../includes/footer/footerNoConnexion/footerAccueil.php'); ?>
+
+</body>
+</html>
