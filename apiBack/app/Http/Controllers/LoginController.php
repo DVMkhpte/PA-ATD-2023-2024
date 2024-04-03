8567ba77b0347fc4501171eabafb9fd3403b52ea
DVM_khpte <147807946+DVMkhpte@users.noreply.github.com>
2024-04-03 15:49:09 +0200

    Now LoginController send role

--- apiBack/app/Http/Controllers/LoginController.php
+++ apiBack/app/Http/Controllers/LoginController.php
@@ -12,34 +12,34 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function store(Request $request)
-    { 
+    {
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        
+
         if (Auth::attempt($credentials)) {
             $user = $request->user();
-            
+
             if ($user->role === 'admin') {
-                
+
                 $token = $user->createToken('admin-access-token')->plainTextToken;
 
                 Log::channel('admin_activity')->info("Admin with email {$user->email} logged in successfully");
-                return response()->json(['token' => $token], 200);
+                return response()->json(['token' => $token,'role' => $user->role], 200);
             }else{
 
                 $token = $user->createToken('Personal Access Token')->plainTextToken;
             }
 
             Log::channel('user_activity')->info("User with email {$user->email} logged in successfully");
-            return response()->json(['token' => $token], 200);
+            return response()->json(['token' => $token,'role' => $user->role], 200);
         }
 
-        
+
         Log::channel('user_activity')->info("User with email {$request->input('email')} tried to login but failed");
         return response()->json(['message' => 'Invalid credentials'], 401);
-        
+
     }
 }
 
