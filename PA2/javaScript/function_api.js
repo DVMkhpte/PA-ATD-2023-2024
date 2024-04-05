0f35230c6564ede13922e3febf88fef50e7f4dbe
TitouanChauché <titouanchauche123@gmail.com>
2024-04-05 14:39:28 +0200

    Update function_api.js

--- PA2/javaScript/function_api.js
+++ PA2/javaScript/function_api.js
@@ -1,13 +1,36 @@
 
 
-async function requestApi(formData, method, link){
-    const response = await fetch('http://localhost:8000/api'+ link, {
+async function requestApi(formData, method, link) {
+    const response = await fetch('http://localhost:8000/api' + link, {
         redirect: 'manual',
         method: method,
         headers: {
             'Content-Type': 'application/json',
         },
+
         body: JSON.stringify(formData)
+
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+
+        if(data){
+            return data
+        }
+    }
+}
+
+
+
+async function requestApiNoBody(method, link) {
+    const response = await fetch('http://localhost:8000/api' + link, {
+        redirect: 'manual',
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+
     });
 
     if (response.ok) {
