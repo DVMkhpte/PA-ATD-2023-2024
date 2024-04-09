bc6af43f4da3c55deebd0c9684801afefdcb21cf
TitouanChauché <titouanchauche123@gmail.com>
2024-04-09 10:25:38 +0200

    Add newPage directory

---
+++ PA2/pages/newPage/addActivitee.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Bénéficiaire</title>
+    <link rel="stylesheet" href="../../css/add.css">
+    <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
+            rel="stylesheet"
+    />
+    <link
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+            rel="stylesheet"
+    />
+</head>
+<body>
+    <div class="container">
+        <div id="leftPart">
+            <h1 class="title">NOUVEAU</h1>
+            <a href="../back_end.php" class="btn-link connexion">Retour</a>
+        </div>
+        <div id="add">
+            <h1 class="title">Ajouter une activitée</h1>
+            <p class="paragraphe">
+                Veuillez remplir tous les champs
+            </p>
+            <form class="formulaire" id="createForm">
+                <div class="group-form">
+                    <input type="text" id="nom" placeholder="Nom">
+                </div>
+                <div class="group-form">
+                    <input type="text" id="type" placeholder="Type">
+                </div>
+                <div class="group-form">
+                    <input type="text" id="description" placeholder="Description">
+                </div>
+                <div class="group-form">
+                    <input type="date" id="date_debut" placeholder="Date de debut">
+                    <input type="date" id="date_fin" placeholder="Date de fin">
+                </div>
+                <div class="group-form">
+                    <input type="number" id="nb_place" placeholder="Nombres de places">
+                    <select class="boutton" id="user" name="select">
+
+                    </select>
+                </div>
+                <div class="group-form">
+                    <input type="submit" class="add" value="Ajouter">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+<script src="../../javaScript/function_api.js"></script>
+<script>
+    //var data = await requestApiNoBody("GET", "/users/")
+    var data = [{
+        "id": 1,
+        "name": "enzo",
+        "code_postal": 91330,
+        "ville": "yerres",
+        "adresse": "affichage",
+        "num_phone": "1234567891",
+        "email": "test@test.fr",
+        "role": "benevole",
+        "email_verified": "False",
+    },
+        {
+            "id": 2,
+            "name": "titouan",
+            "code_postal": 91330,
+            "ville": "yerres",
+            "adresse": "affichage",
+            "num_phone": "1234567891",
+            "email": "test@test.fr",
+            "role": "benevole",
+            "email_verified": "False",
+        }
+    ];
+
+    var addOption = document.getElementById("user")
+    var allUseursName =
+        "<option selected disabled hidden id=\"choix\">Superviser par</option>" +
+        "<option value=\"Null\">à definir</option>"
+    var userName = ""
+
+    for (i = 0; i < data.length; i++) {
+        usersName = "<option value=\"" + data[i].id + "\">" + data[i].name + "</option>";
+        allUseursName = allUseursName.concat(usersName);
+    }
+    addOption.innerHTML = allUseursName
+
+
+    document.getElementById('createForm').addEventListener('submit', async function(event) {
+        event.preventDefault();
+
+        const nom = document.getElementById('nom').value;
+        const description = document.getElementById('description').value;
+        const date_debut = document.getElementById('date_debut').value;
+        const date_fin = document.getElementById('date_fin').value;
+        const nb_place = document.getElementById('nb_place').value;
+        const idUser = document.getElementById('user').value;
+
+
+        const formData = {
+            nom: nom,
+            description: description,
+            date_debut: date_debut,
+            date_fin: date_fin,
+            nb_place: nb_place,
+            no: idUser
+        };
+
+        console.log(formData)
+
+        /*
+        try {
+            const response = await requestApi(formData, "POST", "/activitees/add");
+            if (response.status === 200) {
+                showAlert("Création de l'activitée réussie !");
+            } else {
+                showAlert("Erreur lors de la création de l'utilisateur : " + response.status);
+            }
+        } catch (error) {
+            showAlert('Erreur lors de la requête à l\'API : ' + error.message);
+        }
+        */
+    });
+</script>
+</html>
---
+++ PA2/pages/newPage/addUser.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Bénéficiaire</title>
+    <link rel="stylesheet" href="../../css/add.css">
+    <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
+            rel="stylesheet"
+    />
+    <link
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+            rel="stylesheet"
+    />
+</head>
+<body>
+<div class="container">
+    <div id="leftPart">
+        <h1 class="title">NOUVEAU</h1>
+        <a href="../back_end.php" class="btn-link connexion">Retour</a>
+    </div>
+    <div id="add">
+        <h1 class="title">Ajouter un utilisateur</h1>
+        <p class="paragraphe">
+            Veuillez remplir tous les champs
+        </p>
+        <form class="formulaire" id="createForm">
+            <div class="group-form">
+                <input type="text" id="prenom" placeholder="Prenom">
+                <input type="text" id="nom" placeholder="Nom">
+            </div>
+            <div class="group-form">
+                <input type="text" id="code_postal" placeholder="Code Postal">
+                <input type="text" id="ville" placeholder="Ville">
+            </div>
+            <div class="group-form">
+                <input type="text" id="adresse" placeholder="Adresse">
+            </div>
+            <div class="group-form">
+                <input type="text" id="phone" placeholder="Numero de telephone">
+            </div>
+            <div class="group-form">
+                <input type="email" id="email" placeholder="Mail">
+            </div>
+            <div class="group-form">
+                <input type="password" id="password" placeholder="Mot de passe">
+                <p id="password-error-msg" style="color: red; display: none;">Le mot de passe doit avoir au moins 8 caractères et inclure au moins un chiffre</p>
+            </div>
+            <div class="group-form">
+                <input type="submit" class="add" value="Ajouter">
+            </div>
+        </form>
+    </div>
+</div>
+
+</body>
+<script src="../../javaScript/function_api.js"></script>
+<script>
+
+    document.getElementById("phone").addEventListener("input", function(event) {
+        var input = event.target.value;
+        console.log(input);
+        var sanitizedInput = input.replace(/\D/g, '');
+        event.target.value = sanitizedInput;
+        if (sanitizedInput.length > 10) {
+            event.target.value = sanitizedInput.slice(0, 10);
+        }
+    });
+
+    document.getElementById("password").addEventListener("input", function(event) {
+        var password = event.target.value;
+        var minLength = 8;
+        var hasNumber = /\d/.test(password);
+
+        if (password.length < minLength || !hasNumber) {
+
+            document.getElementById("password-error-msg").style.display = "block";
+            submitButton.disabled = true;
+        } else {
+            document.getElementById("password-error-msg").style.display = "none";
+            submitButton.disabled = false;
+        }
+    });
+
+    document.getElementById('createForm').addEventListener('submit', async function(event) {
+        event.preventDefault();
+
+        const prenom = document.getElementById('prenom').value;
+        const nom = document.getElementById('nom').value;
+        const code_postal = document.getElementById('code_postal').value;
+        const ville = document.getElementById('ville').value;
+        const adresse = document.getElementById('adresse').value;
+        const phone = document.getElementById('phone').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const name = prenom + ' ' + nom;
+        const role = 'beneficiaire';
+
+        const formData = {
+            name: name,
+            code_postal: code_postal,
+            ville: ville,
+            adresse: adresse,
+            num_telephone: phone,
+            email: email,
+            password: password,
+            role: role
+        };
+
+        try {
+            const response = await requestApi(formData, "POST", "/user/create");
+            if (response.status === 200) {
+                showAlert("Création de l'utilisateur réussie !");
+            } else {
+                showAlert("Erreur lors de la création de l'utilisateur : " + response.status);
+            }
+        } catch (error) {
+            showAlert('Erreur lors de la requête à l\'API : ' + error.message);
+        }
+    });
+</script>
+</html>
