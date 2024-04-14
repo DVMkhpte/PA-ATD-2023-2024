de38937ce493a1497998ed44ba5766aba5141785
TitouanChauché <titouanchauche123@gmail.com>
2024-04-14 17:26:12 +0200

    update benevole pages

--- PA2/pages/benevole.php
+++ PA2/pages/benevole.php
@@ -26,126 +26,9 @@
 
 
         <div id="container_2">
-            <div class="filtre">
-                
-                <div  class="barre_de_recherche">
-                    <input type="text" id="search-article-input" placeholder="Formation">
-                </div>
-
-                <div  class="tout_les_filtre">
-                    <select class="boutton" name="trie" id="trie">
-                        <option selected disabled hidden id="choix">Trier par</option>
-                        <option value="nom" onclick="trie('nom')">Nom</option>
-                        <option value="prenom" onclick="trie('type')">Type</option>
-                        <option value="acces" onclick="trie('lieu')">Lieu</option>
-                        <option value="acces" onclick="trie('date')">Date</option>
-                    </select>
-                </div>
-            
-            </div>
-
-            <div class="all_info">
-
-                <div class="info">
-
-                    <div class="description_general">
-                        <div class="description_1">
-                            <div class="description_1_1">
-                                <div class="nom">Nom</div>
-                                <div class="prenom">Type</div>
-                            </div>
-                            <div class="description_1_2">
-                                <div class="acces">lieu</div>
-                                <div class="statut">Date</div>
-                            </div>
-                        </div>
-                        <div class="description_2">
-                            <div class="desription">Desription : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, id. Facilis, accusamus qui. Itaque laudantium laboriosam totam dignissimos voluptates debitis explicabo, in hic officiis error obcaecati, quis minus ipsam similique.</div>
-                        </div>
-                        
-                    </div>
-                    <div class="option">
-                        <button onclick="formationBenevole()" class="inscription" >Ce positionner</button>
-                    </div>
-                    
-                </div>
-
-                <div class="info">
-
-                    <div class="description_general">
-                        <div class="description_1">
-                            <div class="description_1_1">
-                                <div class="nom">Nom</div>
-                                <div class="prenom">Type</div>
-                            </div>
-                            <div class="description_1_2">
-                                <div class="acces">lieu</div>
-                                <div class="statut">Date</div>
-                            </div>
-                        </div>
-                        <div class="description_2">
-                            <div class="desription">Desription : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, id. Facilis, accusamus qui. Itaque laudantium laboriosam totam dignissimos voluptates debitis explicabo, in hic officiis error obcaecati, quis minus ipsam similique.</div>
-                        </div>
-                        
-                    </div>
-                    <div class="option">
-                        <button onclick="formationBenevole()" class="inscription" >Ce positionner</button>
-                    </div>
-                    
-                </div>
-
-                <div class="info">
-
-                    <div class="description_general">
-                        <div class="description_1">
-                            <div class="description_1_1">
-                                <div class="nom">Nom</div>
-                                <div class="prenom">Type</div>
-                            </div>
-                            <div class="description_1_2">
-                                <div class="acces">lieu</div>
-                                <div class="statut">Date</div>
-                            </div>
-                        </div>
-                        <div class="description_2">
-                            <div class="desription">Desription : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, id. Facilis, accusamus qui. Itaque laudantium laboriosam totam dignissimos voluptates debitis explicabo, in hic officiis error obcaecati, quis minus ipsam similique.</div>
-                        </div>
-                        
-                    </div>
-                    <div class="option">
-                        <button onclick="formationBenevole()" class="inscription" >Ce positionner</button>
-                    </div>
-                    
-                </div>
-
-                <div class="info">
-
-                    <div class="description_general">
-                        <div class="description_1">
-                            <div class="description_1_1">
-                                <div class="nom">Nom</div>
-                                <div class="prenom">Type</div>
-                            </div>
-                            <div class="description_1_2">
-                                <div class="acces">lieu</div>
-                                <div class="statut">Date</div>
-                            </div>
-                        </div>
-                        <div class="description_2">
-                            <div class="desription">Desription : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, id. Facilis, accusamus qui. Itaque laudantium laboriosam totam dignissimos voluptates debitis explicabo, in hic officiis error obcaecati, quis minus ipsam similique.</div>
-                        </div>
-                        
-                    </div>
-                    <div class="option">
-                        <button onclick="formationBenevole()" class="inscription" >Ce positionner</button>
-                    </div>
-                    
-                </div>
-
-
-            </div>
 
         </div>
+
     </div>
 
     <div class="profil_part">
@@ -180,5 +63,10 @@
 <?php // include("../includes/footer.php") ?>
 
 <script src="../javaScript/benevole.js"></script>
+<script src="../javaScript/volonteer_function/mission.js"></script>
+<script src="../javaScript/volonteer_function/formation.js"></script>
+<script>
+    affichageBenevole('Formation')
+</script>
 </body>
 </html>
