abd7bcf8cb2276b71aa893e95c071a082f8ea1f5
TitouanChauché <titouanchauche123@gmail.com>
2024-05-01 18:53:03 +0200

    update le createur de trajet

--- PA2/javaScript/trajet.js
+++ PA2/javaScript/trajet.js
@@ -1,3 +1,170 @@
-async function newTrajet(){
+
+async function newTrajet(origin, arrivee, tabEtapes){
+    var tabEtapesTrie = [origin]
+    var depart, distance, distanceMin, adresseMin
+
+    var nbEtape = tabEtapes.length
+    for(i=0; i<nbEtape; i++) {
+        distanceMin = await getDistance(tabEtapesTrie[0], tabEtapes[0])
+        distanceMin = parseFloat(distanceMin.match(/[\d,\.]+/)[0].replace(",", "."));
+        adresseMin = tabEtapes[0]
+        depart=tabEtapesTrie[i]
+
+        for (j = 0; j < tabEtapes.length; j++){
+            distance = await getDistance(depart, tabEtapes[j])
+            distance = parseFloat(distance.match(/[\d,\.]+/)[0].replace(",", "."));
+
+            if(distance < distanceMin){
+                distanceMin = distance
+                adresseMin = tabEtapes[j]
+            }
+        }
+        tabEtapes = tabEtapes.filter(element => element !== adresseMin);
+        tabEtapesTrie.push(adresseMin)
+    }
+
+    tabEtapesTrie.push(arrivee)
+
+    return tabEtapesTrie
+}
+
+async function newCollecte(){
+    const newForm = document.getElementById("mainStock")
+    const dataEntrepot = await requestApiNoBody("GET", "/entrepots/")
+    const dataCommercants = await requestApiNoBody("GET", "/commercants/")
+
+    var form =
+        "<div class='newTrajet'>" +
+        "   <div class='newFormTrajet'>" +
+        "       <select class=\"selectEntrepot\" name=\"status\" id=\"entrepotOrigin\" required>\n" +
+        "           <option selected disabled hidden id=\"entrepot\" >Entrepot Origin</option>\n"+
+        "           <option value='"+ dataEntrepot[0].adresse +"'>"+ dataEntrepot[0].nom +"</option>" +
+        "           <option value='"+ dataEntrepot[1].adresse +"'>"+ dataEntrepot[1].nom +"</option>"+
+        "       </select>\n"+
+        "       <div>Selectionnez les etapes :</div>"+
+        "       <div class='allCheckbox'>"
+    for(i=0; i<dataCommercants.length; i++) {
+        form +=
+        "    <div class='checkbox'>"+
+        "           <input class='checkboxCommercants' type=\"checkbox\" id='"+ dataCommercants[i].id +"' name='"+ dataCommercants[i].id +"'>\n" +
+        "           <label for='"+ dataCommercants[i].id +"'>"+ dataCommercants[i].nom +"</label>" +
+        "   </div>"
+    }
+    form +=
+        "       </div>" +
+        "       <select class=\"selectEntrepot\" name=\"status\" id=\"entrepotDestination\" required>\n" +
+        "           <option selected disabled hidden id=\"entrepotDestination\" >Entrepot destination</option>\n" +
+        "           <option value='" + dataEntrepot[0].adresse + "'>" + dataEntrepot[0].nom + "</option>" +
+        "           <option value='" + dataEntrepot[1].adresse + "'>" + dataEntrepot[1].nom + "</option>" +
+        "       </select>\n"+
+        "       <input class=\"inputDate\" type=\"datetime-local\" id=\"date\" required>" +
+        "       <div class='validTrajet'>"+
+        "           <button class='validTrajetButton' onclick='validCollect()'>Generer le trajet</button>" +
+        "        </div>"+
+        "   </div>" +
+        "   <div id='recap'>" +
+        "       "+
+        "   </div>" +
+        "</div>"
+
+    newForm.innerHTML = form
+}
+async function validCollect(){
+    const dataCommercants = await requestApiNoBody("GET", "/commercants/")
+
+    const origines = document.getElementById("entrepotOrigin").value
+    const destination = document.getElementById("entrepotDestination").value
+    const date = document.getElementById("date").value
+    var etapes = []
+    for(i=0; i<dataCommercants.length; i++){
+        var checkboxValue = document.getElementById(dataCommercants[i].id).checked;
+        if (checkboxValue) {
+            etapes.push(dataCommercants[i].adresse)
+        }
+    }
+
+    var tabEtapesTrie = await newTrajet(origines, destination, etapes)
+
+
+    const addReacap = document.getElementById("recap")
+
+    var nbEtape = tabEtapesTrie.length
+    var recap =
+        "<div id='pdf'>"+
+        "   <h2 id='titre' > Nouvelle collecte </h2>" +
+        "   <h3 id='feuilleDeRoutes'>Feuille de route</h3>" +
+        "   <div id='routes'>" +
+        "       Pour le : " +
+        "       <div id='date'>"+ date +"</div> "+
+        "       <div id='depart'>Depart : "+ tabEtapesTrie[0] +"</div>"
+    for(i=1; i<nbEtape -1; i++) {
+        recap += "<div id='etape" + i + "'>Etape " + i + " : " + tabEtapesTrie[i] + "</div>"
+    }
+
+    recap +=
+        "       <div id='destination'>Destination : "+ tabEtapesTrie[nbEtape -1] +"</div>"+
+        "   </div>" +
+        "</div>" +
+        "<div class='option trajet'>" +
+        "   <button class='addNewTrajet' onclick='validTrajet("+ (nbEtape - 1) +", \"collecte\")'>Valider cette collecte</button>" +
+        "</div>"
+
+    addReacap.innerHTML = recap
+
 
 }
+
+async function newMaraude(){
+
+}
+
+
+
+
+
+async function validTrajet(nbEtapes, type){
+    var textAsk = ""
+    var getText
+    var textPdf = document.getElementById("pdf")
+
+    var pdfName = 'trajet-'+ type +'.pdf'
+    var opt = {
+        margin:  0.5,
+        filename:     'trajet.pdf',
+        image:        { type: 'jpeg', quality: 1 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().set(opt).from(textPdf).save()
+
+
+    getText = document.getElementById("depart")
+    textAsk += getText.innerText+"  "
+
+    for(i=1; i<nbEtapes; i++){
+        getText = document.getElementById("etape"+i)
+        textAsk += getText.innerText+"  "
+    }
+    getText = document.getElementById("destination")
+    textAsk += getText.innerText+"  "
+
+    var date = document.getElementById("date").value
+
+    const formData = {
+        type: "collecte",
+        demande: textAsk,
+        permis: "0",
+        etat : "valide",
+        date: date
+    };
+    console.log(formData)
+    try {
+        const response = await requestApi(formData, "POST", "/demande/add");
+        showAlert("Création de la demande !");
+
+    } catch (error) {
+        showAlert('Erreur lors de la requête à l\'API : ' + error.message);
+    }
+}
+
+
--- PA2/pages/stock.php
+++ PA2/pages/stock.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Bénévole</title>
     <link rel="stylesheet" href="../CSS/stock.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
 </head>
 <body>
 <header>
@@ -14,12 +15,12 @@
         <a class="nav_link_acceuil" href="back_end.php">&lt;&lt;Retour</a>
     </div>
     <div class="mid_partH">
-        <h1>Gestion des stock</h1>
+        <h1>Gestion des entrepots</h1>
     </div>
     <div class="right_partH">
         <div id="option">
-            <button class="new_button" id="new_collect">Nouvelle Collecte</button>
-            <button class="new_button" id="new_maraude">Nouvelle Maraude</button>
+            <button class="new_button" id="new_collect" onclick="newCollecte()">Nouvelle Collecte</button>
+            <button class="new_button" id="new_maraude" onclick="newMaraude()">Nouvelle Maraude</button>
         </div>
     </div>
 </header>
@@ -47,40 +48,36 @@
 </main>
 </body>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6CUzOUwOwUUoHYp7s5ZFpow70ao6eC_0=places"></script>
-    <script>
-    // Initialisation du service de la matrice de distances
-    var distanceMatrixService = new google.maps.DistanceMatrixService();
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRgUYpS2R7KL3eZeSGVQYj8Gs9_lVy0x0&libraries=places"></script>
+<script>
+    async function getDistance(origin, destination) {
+        return new Promise((resolve, reject) => {
+            // Initialisation du service de la matrice de distances
+            var distanceMatrixService = new google.maps.DistanceMatrixService();
 
-    // Définir les adresses de départ et d'arrivée
-    var origin = '5 Rue Jules Ferry, 91330 Yerres';
-    var destination = 'Parc Astérix, 60128 Plailly';
+            // Paramètres de la requête
+            var request = {
+                origins: [origin],
+                destinations: [destination],
+                travelMode: 'DRIVING',
+                unitSystem: google.maps.UnitSystem.METRIC,
+                avoidHighways: false,
+                avoidTolls: false
+            };
 
-    // Paramètres de la requête
-    var request = {
-        origins: [origin],
-        destinations: [destination],
-        travelMode: 'DRIVING', // Mode de transport (DRIVING, WALKING, BICYCLING, TRANSIT)
-        unitSystem: google.maps.UnitSystem.METRIC, // Unité de mesure (METRIC, IMPERIAL)
-        avoidHighways: false, // Éviter les autoroutes
-        avoidTolls: false // Éviter les péages
-    };
-
-    // Envoi de la requête au service de la matrice de distances
-    distanceMatrixService.getDistanceMatrix(request, function(response, status) {
-        if (status == 'OK') {
-            // Récupérer la distance et la durée
-            var distance = response.rows[0].elements[0].distance.text;
-            var duration = response.rows[0].elements[0].duration.text;
-
-            // Afficher les résultats
-            console.log('Distance:', distance);
-            console.log('Durée:', duration);
-        } else {
-            console.log('Erreur:', status);
-        }
-    });
+            distanceMatrixService.getDistanceMatrix(request, function (response, status) {
+                if (status == 'OK') {
+                    // Récupérer la distance
+                    var distance = response.rows[0].elements[0].distance.text;
+                    resolve(distance); // Résoudre la promesse avec la distance
+                } else {
+                    reject('Erreur:' + status); // Rejeter la promesse avec le statut d'erreur
+                }
+            });
+        });
+    }
 </script>
 <script src="../javaScript/function_api.js"></script>
+<script src="../javaScript/trajet.js"></script>
 <script src="../javaScript/stock.js"></script>
 </html>
