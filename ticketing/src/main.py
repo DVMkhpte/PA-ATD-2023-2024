dd2080ea70b5fdbee4f6a3b347a14faf4a01d69d
Ryatozz <ryatozz@gmail.com>
2024-05-08 18:06:35 +0200

    start connection to api on ticketing app

--- ticketing/src/main.py
+++ ticketing/src/main.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 import re
+import requests
+import json
 from PIL import Image, ImageTk
 from admin import AdminTicketingApp
 from user import TicketingApp
+from tkinter import messagebox
 
 class LoginPage:
     def __init__(self, root):
@@ -10,15 +13,13 @@ class LoginPage:
         self.root.title("Connexion - App Ticketing Au Temps Donné")
         self.root.geometry("600x500")
 
+        self.bg_color = "#f0f0f0"
+        self.button_color = "#007bff"
+        self.button_text_color = "white"
 
-        bg_color = "#f0f0f0"
-        button_color = "#007bff"
-        button_text_color = "white"
-
-
-        button_style = {
-            "bg": button_color,
-            "fg": button_text_color,
+        self.button_style = {
+            "bg": self.button_color,
+            "fg": self.button_text_color,
             "font": ("Arial", 12),
             "relief": "flat",
             "activebackground": "#0056b3",
@@ -28,10 +29,8 @@ class LoginPage:
             "width": 15
         }
 
-
-        main_frame = tk.Frame(root, bg=bg_color)
-        main_frame.pack(expand=True, fill=tk.BOTH)
-
+        self.main_frame = tk.Frame(root, bg=self.bg_color)
+        self.main_frame.pack(expand=True, fill=tk.BOTH)
 
         self.root.update_idletasks()
         window_width = self.root.winfo_width()
@@ -44,50 +43,53 @@ class LoginPage:
         original_image = Image.open(image_path)
         resized_image = original_image.resize((200, 200))
         self.logo_image = ImageTk.PhotoImage(resized_image)
-        logo_label = tk.Label(main_frame, image=self.logo_image, bg=bg_color)
+        logo_label = tk.Label(self.main_frame, image=self.logo_image, bg=self.bg_color)
         logo_label.pack(pady=20)
 
-
-        tk.Label(main_frame, text="Adresse email:", bg=bg_color).pack()
-        self.email_entry = tk.Entry(main_frame, bg="white")
+        tk.Label(self.main_frame, text="Adresse email:", bg=self.bg_color).pack()
+        self.email_entry = tk.Entry(self.main_frame, bg="white")
         self.email_entry.pack(pady=5)
-        tk.Label(main_frame, text="Mot de passe:", bg=bg_color).pack()
-        self.password_entry = tk.Entry(main_frame, show="*", bg="white")
+        tk.Label(self.main_frame, text="Mot de passe:", bg=self.bg_color).pack()
+        self.password_entry = tk.Entry(self.main_frame, show="*", bg="white")
         self.password_entry.pack(pady=5)
 
-
-        login_button = tk.Button(main_frame, text="Se connecter", command=self.login, **button_style)
+        login_button = tk.Button(self.main_frame, text="Se connecter", command=self.login, **self.button_style)
         login_button.pack(pady=10)
+
+    
 
     def login(self):
         email = self.email_entry.get().strip()
-        password = self.password_entry.get().strip() 
+        password = self.password_entry.get().strip()
 
-        if not email or not password:
-            tk.messagebox.showerror("Erreur de connexion", "Veuillez remplir tous les champs.")
-            return
-
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            tk.messagebox.showerror("Erreur de connexion", "Veuillez saisir une adresse email valide.")
-            return
-
-        if len(password) < 8:
-            tk.messagebox.showerror("Erreur de connexion", "Le mot de passe doit contenir au moins 8 caractères.")
-            return
-
-        login_data = {
-        "email": email,
-        "password": password
+        # Convertir les données du formulaire en JSON
+        form_data = {
+            "email": email,
+            "password": password
         }
+        form_json = json.dumps(form_data)
 
-        role_data = {
-            "token": "62|ZJ6BNqHBLbB6NgpTOCalsdu7KDP1p4c2cgZn3E7e",
-            "role": "benevole",
-            "id": 3
-        }
-        role = role_data["role"]
+        # Imprimer les données du formulaire au format JSON
+        print("Données du formulaire (JSON) :", form_json)
 
-        # Redirection en fonction du rôle
+        try:
+            response = requests.post("http://localhost:8000/api/user/login", json=form_data)
+
+            print(response)  # Ajout du print pour afficher la réponse
+
+            if response.status_code == 200:
+                user_data = response.json()
+                self.redirect_based_on_role(user_data)
+            else:
+                messagebox.showerror("Erreur de connexion", "Identifiants incorrects")
+        except Exception as e:
+            messagebox.showerror("Erreur de connexion", f"Une erreur s'est produite : {e}")
+
+
+
+
+    def redirect_based_on_role(self, user_data):
+        role = user_data.get("role")
         if role == "admin":
             self.root.destroy()
             root = tk.Tk()
@@ -96,10 +98,10 @@ class LoginPage:
         elif role == "beneficiaire" or role == "benevole":
             self.root.destroy()
             root = tk.Tk()
-            admin_app = TicketingApp(root)
+            user_app = TicketingApp(root)
             root.mainloop()
         else:
-            tk.messagebox.showerror("Erreur de connexion", "Identifiants incorrects")
+            messagebox.showerror("Erreur de connexion", "Rôle utilisateur non valide")
 
 
 if __name__ == "__main__":
