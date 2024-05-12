e089e1f779eb0a27ffa1d356afc9d08af1e6e2bd
DVM_khpte <147807946+DVMkhpte@users.noreply.github.com>
2024-05-12 21:00:57 +0200

    test htaccess v2

--- PA2/erreur.php
+++ PA2/erreur.php
@@ -1,7 +1,13 @@
+<?php
+
+$erreur = $_GET['erreur'];
+
+?>
+
 <div id="main">
     <div class="fof">
-        <h1>Error <?php echo http_response_code();?></h1>
-        <h2><a href="../index.php">---->    Back to home    <----</a></h2>
+        <h1>Error <?php echo $erreur ?></h1>
+        <h2><a href="pages/index.php">---->    Back to home    <----</a></h2>
     </div>
 </div>
 
